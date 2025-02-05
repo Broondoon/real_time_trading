0191d34a9cbd56b799c9871ed87f157fbef389cd
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2025-02-05 13:55:57 -0800

    Basic App Setup
    Creating a few folders, creating blank pages.

---
+++ flutter_frontend/lib/router/app_router.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+const String loginRouteName = 'login';
+const String homeRouteName = 'home';
+const String marketRouteName = 'market';
+
