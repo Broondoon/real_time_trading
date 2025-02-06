7be145cfad4de125aca44fc0f624b5b63199a99d
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2025-02-05 21:29:58 -0800

    Router + Refactoring Pages
    Router exists now! However it's not really being used for much.

--- flutter_frontend/lib/router/app_router.dart
+++ flutter_frontend/lib/router/app_router.dart
@@ -1,7 +1,55 @@
+// File initially generated via Gemini 2.0 Flash Experimental.
+// Manually typed out and edited for my own understanding.
 import 'package:flutter/material.dart';
+import 'package:flutter_frontend/main.dart';
+import 'package:flutter_frontend/views/auth/auth_controller.dart';
+import 'package:flutter_frontend/views/auth/login_page_view.dart';
+import 'package:flutter_frontend/views/home/home_page_view.dart';
+import 'package:flutter_frontend/views/market/market_page_view.dart';
 import 'package:go_router/go_router.dart';
+import 'package:provider/provider.dart';
 
 const String loginRouteName = 'login';
 const String homeRouteName = 'home';
 const String marketRouteName = 'market';
 
+final goRouter = GoRouter(
+  initialLocation: '/',
+  redirect: (context, state) {
+    final authController = Provider.of<AuthController>(
+      context,
+      listen: false
+    );
+    final isLoggedIn = authController.isLoggedIn;
+    final isLoggingIn = state.uri.toString() == '/login';
+
+    // If we're not logged in, and not already at the login page, go to the login page.
+    if (!isLoggedIn && !isLoggingIn) {
+      return '/login';
+    }
+
+    // If we're logged in @ the login page, go to /home.
+    if (isLoggedIn && isLoggingIn) {
+      return '/home';
+    }
+
+    return null;
+  },
+  routes: [
+    GoRoute(
+      path: '/',
+      builder: (context, state) => const HomePage(),
+      name: homeRouteName,
+    ),
+    GoRoute(
+      path: '/login',
+      builder: (context, state) => const LoginPage(),
+      name: loginRouteName,
+    ),
+    GoRoute(
+      path: '/market',
+      builder: (context, state) => const MarketPage(),
+      name: marketRouteName,
+    ),
+  ],
+);
