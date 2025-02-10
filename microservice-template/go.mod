6e93d7b25c59a007e712e112c3346eff36cdb727
KyleSullivanGitHub <kyle@kylesullivan.me>
2025-02-10 13:48:24 -0800

    Basic Template Project Ready

--- microservice-template/go.mod
+++ microservice-template/go.mod
@@ -1,3 +1,3 @@
-module main
+module template
 
 go 1.23.5
--- microservice-template/main.go
+++ microservice-template/main.go
@@ -1,9 +1,30 @@
 package main
 
 import (
+	"Shared/entities/entity"
+	"Shared/entities/user"
 	"fmt"
+	"time"
 )
 
 func main() {
-	fmt.Println("Hello, World!");
+	fmt.Println("Hello, World!")
+
+	u := user.NewUser(user.NewUserParams{
+		NewEntityParams: entity.NewEntityParams{
+			Id:           "u1",
+			DateCreated:  time.Now(),
+			DateModified: time.Now(),
+		},
+		Username: "test",
+		Password: "password",
+	})
+
+	// Print the User
+	fmt.Print("User: ")
+	fmt.Println(u.GetId())
+	fmt.Println(u.GetDateCreated())
+	fmt.Println(u.GetDateModified())
+	fmt.Println(u.GetUsername())
+	fmt.Println(u.GetPassword())
 }
