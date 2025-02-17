85bb4dd7de920f39071f812cd6d9a73953c989e5
Brendan Wadey <62085968+Broondoon@users.noreply.github.com>
2025-02-16 16:24:18 -0800

    Docker Communication Fixing More
    Now CORS is the blocker...

--- flutter_frontend/lib/views/auth/auth_controller.dart
+++ flutter_frontend/lib/views/auth/auth_controller.dart
@@ -1,5 +1,7 @@
 // File initially generated via Gemini 2.0 Flash Experimental.
 // Manually typed out and edited for my own understanding.
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_frontend/api_service.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
@@ -14,22 +16,39 @@ class AuthController extends ChangeNotifier {
   bool get isLoggedIn => _token != null;
 
   // TODO: https
-  final String _baseUrl = 'http://localhost:3001/';
+  final String _baseUrl = 'http://127.0.0.1:3001/';
   
   // Class objects; these could be dependency injected, no? Something to think about in the future.
   late Dio _dio;
+  late Dio _unauthDio;
   // late APIService _apiService;
 
   // Lifetime should be 1 hour
   final int _tokenLifetime = 3600;
   DateTime? _tokenExpiration;
 
+  // REGISTER_RESPONSE=$(curl -s -X POST "$BASE_URL/authentication/register" -H "Content-Type: application/json" \
+  //   -d "{\"username\": \"$USERNAME\", \"password\": \"$PASSWORD\"}")
+
   AuthController() {
     _dio = Dio(
       BaseOptions(
         baseUrl: _baseUrl,
+        connectTimeout: const Duration(seconds: 10),
+        receiveTimeout: const Duration(seconds: 10),
       )
     );
+
+    _unauthDio = Dio(
+      BaseOptions(
+        baseUrl: _baseUrl,
+      )
+    );
+
+    // TODO: would be nicer if this could be done in the instantiation rather than here
+    // although, this is default value
+    _dio.options.contentType = Headers.jsonContentType;
+    // _unauthDio.options.
 
     // _apiService = APIService(
     //   _dio,
@@ -46,6 +65,9 @@ class AuthController extends ChangeNotifier {
             options.headers['Authorization'] = 'Bearer $_token';
           }
           else {
+            // if NOT /login or /register, then kill
+            
+
             //TODO: re-enable auto-logout
             print("Logout redirect!");
             logout();
@@ -109,12 +131,15 @@ class AuthController extends ChangeNotifier {
   Future<bool> login(String username, String pwd) async {
     try {
       print("Making login request:");
-      final response = await _dio.post(
+
+      var reqData = {
+        'username': username,
+        'password': pwd,
+      };
+
+      final response = await _unauthDio.post(
         '/authentication/login', // TODO: replace this with a /resources/app_strings reference instead
-        data: {
-          'username': username,
-          'password': pwd,
-        },
+        data: jsonEncode(reqData),
       );
 
       // I now realize this is functionally USELESS until I create some unique
@@ -163,7 +188,7 @@ class AuthController extends ChangeNotifier {
     Future<bool> register(String username, String pwd) async {
     try {
       print("Making registration request:");
-      final response = await _dio.post(
+      final response = await _unauthDio.post(
         '/authentication/register', // TODO: replace this with a /resources/app_strings reference instead
         data: {
           'username': username,
