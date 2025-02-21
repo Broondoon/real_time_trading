498f44abf6dbee4937846f081f4605f4e2dadab6
Michael <48280262+miichae1@users.noreply.github.com>
2025-02-20 22:32:10 -0800

    Need to test.

--- order-executor-service/orderExecutor/order-executor-handlers.go
+++ order-executor-service/orderExecutor/order-executor-handlers.go
@@ -12,13 +12,13 @@ import (
 
 var databaseAccessTransact databaseAccessTransaction.DatabaseAccessInterface
 var databaseAccessUser databaseAccessUserManagement.DatabaseAccessInterface
-var networkManager network.NetworkInterface
+//var networkManager network.NetworkInterface
 
 func InitalizeExecutorHandlers(
     networkManager network.NetworkInterface, databaseAccessTransact databaseAccessTransaction.DatabaseAccessInterface, databaseAccessUser databaseAccessUserManagement.DatabaseAccessInterface) {
-    databaseAccessTransact = databaseAccessTransact
+/*     databaseAccessTransact = databaseAccessTransact
     databaseAccessUser = databaseAccessUser
-    networkManager = networkManager
+    networkManager = networkManager */
 
     // Build Handler
     networkManager.AddHandleFuncUnprotected(network.HandlerParams{Pattern: "executor", Handler: executorHandler})
@@ -30,3 +30,29 @@ func healthHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     fmt.Fprintln(w, "OK")
 }
+
+
+func executorHandler(responseWriter http.ResponseWriter, data []byte, queryParams url.Values, requestType string) {
+    // Parse the data
+    var orderData network.MatchingEngineToExecutionJSON
+    err := json.Unmarshal(data, &orderData)
+    if err != nil {
+        responseWriter.WriteHeader(http.StatusBadRequest)
+        return
+    }
+
+    success, err := ProcessTrade(orderData, databaseAccessTransact, databaseAccessUser)
+    if err != nil {
+        responseWriter.WriteHeader(http.StatusInternalServerError)
+        return
+    }
+
+    if success {
+        responseWriter.WriteHeader(http.StatusOK)
+        return
+    }
+
+    responseWriter.WriteHeader(http.StatusResetContent)
+}
+
+
