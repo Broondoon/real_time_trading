a065d2e36f94e7959013314e35898cb58e805d64
miichae1 <48280262+miichae1@users.noreply.github.com>
2025-02-24 22:06:06 -0800

    Updating logic of response to Matching Engine and making things pretty

--- order-executor-service/orderExecutor/order-executor-handlers.go
+++ order-executor-service/orderExecutor/order-executor-handlers.go
@@ -12,24 +12,27 @@ import (
 
 var databaseAccessTransact databaseAccessTransaction.DatabaseAccessInterface
 var databaseAccessUser databaseAccessUserManagement.DatabaseAccessInterface
-//var networkManager network.NetworkInterface
+
+
 
 func InitalizeExecutorHandlers(
-    networkManager network.NetworkInterface, databaseAccessTransact databaseAccessTransaction.DatabaseAccessInterface, databaseAccessUser databaseAccessUserManagement.DatabaseAccessInterface) {
-/*     databaseAccessTransact = databaseAccessTransact
-    databaseAccessUser = databaseAccessUser
-    networkManager = networkManager */
+    networkManager network.NetworkInterface, 
+    databaseAccessTransact databaseAccessTransaction.DatabaseAccessInterface, 
+    databaseAccessUser databaseAccessUserManagement.DatabaseAccessInterface) {
 
-    // Build Handler
     networkManager.AddHandleFuncUnprotected(network.HandlerParams{Pattern: "executor", Handler: executorHandler})
 
     http.HandleFunc("/health", healthHandler)
 }
 
+
+
 func healthHandler(w http.ResponseWriter, r *http.Request) {
     w.WriteHeader(http.StatusOK)
     fmt.Fprintln(w, "OK")
 }
+
+
 
 
 func executorHandler(responseWriter http.ResponseWriter, data []byte, queryParams url.Values, requestType string) {
@@ -40,18 +43,39 @@ func executorHandler(responseWriter http.ResponseWriter, data []byte, queryParam
         return
     }
 
-    success, err := ProcessTrade(orderData, databaseAccessTransact, databaseAccessUser)
+    // Process the orderData (transferEntity) from the Matching Engine
+    buySuccess, sellSuccess, err := ProcessTrade(orderData, databaseAccessTransact, databaseAccessUser)
     if err != nil {
         responseWriter.WriteHeader(http.StatusInternalServerError)
         return
     }
 
-    if success {
-        responseWriter.WriteHeader(http.StatusOK)
+
+    // Independent failure flags //
+    // If the match was successful, both IsBuyFailure and IsSellFailure will be false
+    // If the match was unsuccessful, only one of IsBuyFailure and IsSellFailure will be true 
+    // If there was an error, both IsBuyFailure and IsSellFailure will be true
+    responseEntity := network.ExecutorToMatchingEngineJSON{
+        IsBuyFailure:  !buySuccess,
+        IsSellFailure: !sellSuccess,
+    }
+
+
+    jsonResponseToMatchingEngine, err := json.Marshal(responseEntity)
+    if err != nil {
+        responseWriter.WriteHeader(http.StatusInternalServerError)
         return
     }
 
-    responseWriter.WriteHeader(http.StatusResetContent)
+    //responseWriter.Header().Set("Content-Type", "application/json")
+
+
+
+    // Send a 200 OK response if the request was successful (unrelated to match success)
+    responseWriter.WriteHeader(http.StatusOK)
+
+
+    // Send the info of whether the match was successful or not
+    responseWriter.Write(jsonResponseToMatchingEngine)
+
 }
-
-
